6f25e9468b59ac2f3cbfc829ebbb426958bae717
aiody <aiody329@gmail.com>
2020-07-13 12:43:46 +0900

    ADD 1065_한수.js  - 완전 탐색 알고리즘을 사용하여 한수 문제를 풀었습니다.

---
+++ Search/1065_한수.js
@@ -0,0 +1,42 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+/*
+<문제 풀이 과정>
+주어진 값보다 작은 모든 한수의 개수를 알아내기.
+완전 탐색으로 풀어야겠다.
+
+1부터 주어진 값까지 반복문을 돌면서 한수인지 아닌지 검사하는 방식으로 진행.
+한수인지 아닌지를 알 수 있는 함수는 숫자를 잘라서 그 차이가 같은지 검사함.
+
+*/
+
+let input;
+rl.on('line', (line) => {
+  input = line;
+}).on('close', () => {
+  let count = 0;
+  for (let i = 1; i <= input; i++) {
+    if (isHansu(i)) {
+      count++;
+    }
+  }
+  console.log(count);
+});
+
+function isHansu(num) {
+  let splitNum = String(num)
+    .split('')
+    .map((ele) => parseInt(ele));
+  if (splitNum.length === 1) return true;
+  let diff = splitNum[0] - splitNum[1];
+  for (let i = 0; i < splitNum.length - 1; i++) {
+    if (splitNum[i] - splitNum[i + 1] !== diff) {
+      return false;
+    }
+  }
+  return true;
+}
