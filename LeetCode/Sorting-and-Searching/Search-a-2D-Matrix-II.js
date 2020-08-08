cf970b8168eb42cf75a26a31ce8f2026b84db484
aiody <aiody329@gmail.com>
2020-08-08 17:00:53 +0900

    improve search-a-2D-Matrix-II problem

--- LeetCode/Sorting-and-Searching/Search-a-2D-Matrix-II.js
+++ LeetCode/Sorting-and-Searching/Search-a-2D-Matrix-II.js
@@ -6,12 +6,30 @@
  * @return {boolean}
  */
 var searchMatrix = function (matrix, target) {
-  for (let row = 0; row < matrix.length; row++) {
-    for (let col = 0; col < matrix[0].length; col++) {
-      if (matrix[row][col] === target) {
-        return true;
-      }
+  if (matrix.length === 0) return false;
+  let curRow = 0;
+  let curCol = matrix[0].length - 1;
+
+  while (curRow < matrix.length && curCol >= 0) {
+    if (matrix[curRow][curCol] < target) {
+      curRow++;
+    } else if (matrix[curRow][curCol] > target) {
+      curCol--;
+    } else {
+      return true;
     }
   }
   return false;
+
+  // previous solution
+  /*
+  for(let row = 0; row < matrix.length; row++) {
+      for(let col = 0; col < matrix[0].length; col++) {
+          if(matrix[row][col] === target) {
+              return true;
+          }
+      }
+  }
+  return false;
+  */
 };
