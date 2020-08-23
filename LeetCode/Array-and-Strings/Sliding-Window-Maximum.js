72df1021a29ab69ccb415bbc9fc764b1a7143ae8
aiody <aiody329@gmail.com>
2020-08-23 18:58:41 +0900

    solve again sliding-window-maximum problem by better solution at LeetCOde

--- LeetCode/Array-and-Strings/Sliding-Window-Maximum.js
+++ LeetCode/Array-and-Strings/Sliding-Window-Maximum.js
@@ -6,20 +6,40 @@
  * @return {number[]}
  */
 var maxSlidingWindow = function (nums, k) {
+  // brute force
+  /*
   const answer = [];
-  for (let i = 0; i <= nums.length - k; i++) {
-    answer.push(nums[i]);
+  for(let i = 0; i <= nums.length - k; i++) {
+      answer.push(nums[i]);
   }
+  
+  for(let i = 0; i < nums.length; i++) {
+      for(let j = Math.max(0, i - k + 1); j <= Math.min(answer.length - 1, i); j++) {
+          answer[j] = Math.max(nums[i], answer[j]);
+      }
+  }
+  
+  return answer;
+  */
+
+  // https://leetcode.com/explore/interview/card/top-interview-questions-hard/116/array-and-strings/837/discuss/135919/JavaScript-solution-using-Monotonic-queue
+
+  let queue = [];
+  const answer = [];
 
   for (let i = 0; i < nums.length; i++) {
-    for (
-      let j = Math.max(0, i - k + 1);
-      j <= Math.min(answer.length - 1, i);
-      j++
-    ) {
-      answer[j] = Math.max(nums[i], answer[j]);
+    while (queue.length && nums[i] > queue[queue.length - 1]) {
+      queue.pop();
+    }
+    queue.push(nums[i]);
+
+    let veryLeftIndexOfWindow = i - k + 1;
+    if (veryLeftIndexOfWindow >= 0) {
+      answer.push(queue[0]);
+      if (nums[veryLeftIndexOfWindow] === queue[0]) {
+        queue.shift();
+      }
     }
   }
-
   return answer;
 };
