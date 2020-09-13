9c05be79ec7040b71746694bd143156ded951622
aiody <aiody329@gmail.com>
2020-09-13 21:07:12 +0900

    solve a problem at Baekjoon

---
+++ 백준-알고리즘/String/단어 길이 재기.js
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/2743
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let input;
+rl.on('line', (line) => {
+  input = line;
+}).on('close', () => {
+  console.log(input.length);
+});
